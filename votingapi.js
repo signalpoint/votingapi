4722c0b001d261dfde441efef146a5b0b3bf7238
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-04-27 21:31:13 -0400

    Stringify options before service call in votingapi_select_votes().

--- votingapi.js
+++ votingapi.js
@@ -1,5 +1,5 @@
 /**
- *
+ * The select_votes service resource.
  */
 function votingapi_select_votes(options) {
   try {
@@ -7,6 +7,7 @@ function votingapi_select_votes(options) {
     options.path = 'votingapi/select_votes.json';
     options.service = 'votingapi';
     options.resource = 'select_votes';
+    if (typeof options.data === 'object') { options.data = JSON.stringify(options.data); }
     Drupal.services.call(options);
   }
   catch (error) { console.log('votingapi_select_votes - ' + error); }
@@ -14,14 +15,14 @@ function votingapi_select_votes(options) {
 
 /**
  * The set_votes service resource.
- *   options.data.votes.entity_type  (Optional, defaults to 'node')
- *   options.data.votes.entity_id    (Required)
- *   options.data.votes.value_type    (Optional, defaults to 'percent')
- *   options.data.votes.value         (Required)
- *   options.data.votes.tag           (Optional, defaults to 'vote')
- *   options.data.votes.uid           (Optional, defaults to current user)
- *   options.data.votes.vote_source   (Optional, defaults to current IP)
- *   options.data.votes.timestamp     (Optional, defaults to REQUEST_TIME)
+ *   options.data.votes[0].entity_type  (Optional, defaults to 'node')
+ *   options.data.votes[0].entity_id    (Required)
+ *   options.data.votes[0].value_type    (Optional, defaults to 'percent')
+ *   options.data.votes[0].value         (Required)
+ *   options.data.votes[0].tag           (Optional, defaults to 'vote')
+ *   options.data.votes[0].uid           (Optional, defaults to current user)
+ *   options.data.votes[0].vote_source   (Optional, defaults to current IP)
+ *   options.data.votes[0].timestamp     (Optional, defaults to REQUEST_TIME)
  */
 function votingapi_set_votes(options) {
   try {
